f3c5aa19249b1a07234c99af0afd966cc2d9ec9b
ppppbn <phambaongoc1511@gmail.com>
2018-06-10 01:24:36 +0700

    More backgrounds and transition on load

--- main.js
+++ main.js
@@ -2,76 +2,80 @@ const weather = ['drizzle', 'clouds', 'rain', 'snow', 'clear', 'thunderstom'];
 var unit = "c";
 var degreeInCelcius = 0;
 var degreeInFahrenheit = 0;
-$(document).ready(function(){
-  getLocation();
 
+window.onload = function(){ 
+  $(".cover-bg").fadeOut(500);  
+  getLocation();
+}
+
+$(document).ready(function(){
   $("#unit").click(function(){
     toggleUnit();
   })
 
+  
+})
 
-  function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(getWeather, displayError);
-    } else {
-        $("#geolocation-info").html("Geolocation is not supported by this browser.");
-        $("#loading").fadeOut(200);
-    }
-  }
-
-  function getWeather(position) {
-    $.ajax({
-      url : "https://fcc-weather-api.glitch.me/api/current/",
-      data : {
-        lat : position.coords.latitude.toFixed(8),
-        lon : position.coords.longitude.toFixed(8)
-      },
-      type : "GET",
-      success: function(data){
-        degreeInCelcius = Math.floor(data.main.temp);
-        degreeInFahrenheit = convertFromCToF(degreeInCelcius);
-        $("#loading").fadeOut(200);
-        $("#country").html(data.name + ", " + data.sys.country);
-        $("#temp").html(Math.floor(data.main.temp) + " °");
-        $("#main").html(data.weather[0].main);
-        $("#unit").html(unit);
-        generateIcon(data.weather[0].main);
-        $(".info-container").fadeIn(400);
-      },
-      error: function(err){
-        console.log(err);
-      }
-    }) 
-  }
-
-  function displayError(error){
-    if (error.code == error.PERMISSION_DENIED)
-      $("#geolocation-info").html("You need to enable location services to see your local weather. Reload and try again!");
+function getLocation() {
+  if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(getWeather, displayError);
+  } else {
+      $("#geolocation-info").html("Geolocation is not supported by this browser.");
       $("#loading").fadeOut(200);
   }
+}
 
-  function generateIcon(desc) {
-    desc = desc.toLowerCase();
-    if(desc){
-      if(weather.indexOf(desc) >= 0) $('#' + desc).removeClass('hide');
-      else $('#clouds').removeClass('hide');
+function getWeather(position) {
+  $.ajax({
+    url : "https://fcc-weather-api.glitch.me/api/current/",
+    data : {
+      lat : position.coords.latitude.toFixed(8),
+      lon : position.coords.longitude.toFixed(8)
+    },
+    type : "GET",
+    success: function(data){
+      degreeInCelcius = Math.floor(data.main.temp);
+      degreeInFahrenheit = convertFromCToF(degreeInCelcius);
+      $("#loading").fadeOut(200);
+      $("#country").html(data.name + ", " + data.sys.country);
+      $("#temp").html(Math.floor(data.main.temp) + " °");
+      $("#main").html(data.weather[0].main);
+      $("#unit").html(unit);
+      generateIcon(data.weather[0].main);
+      $(".info-container").fadeIn(400);
+    },
+    error: function(err){
+      console.log(err);
     }
-  }
+  }) 
+}
 
-  function toggleUnit(){
-    unit = unit.toLowerCase() === 'c' ? 'f' : 'c';
-    $("#unit").html(unit);
-    if(unit === 'c') {
-      $("#temp").html(Math.floor(degreeInCelcius) + " °");
-    }
-    else {
-      $("#temp").html(Math.floor(degreeInFahrenheit) + " °");
-    }
-  }
+function displayError(error){
+  if (error.code == error.PERMISSION_DENIED)
+    $("#geolocation-info").html("You need to enable location services to see your local weather. Reload and try again!");
+    $("#loading").fadeOut(200);
+}
 
-  function convertFromCToF(degree){
-    degree = Number(degree);
-    return Math.floor((degree*9 + 160)/5);
+function generateIcon(desc) {
+  desc = desc.toLowerCase();
+  if(desc){
+    if(weather.indexOf(desc) >= 0) $('#' + desc).removeClass('hide');
+    else $('#clouds').removeClass('hide');
   }
+}
 
-})
+function toggleUnit(){
+  unit = unit.toLowerCase() === 'c' ? 'f' : 'c';
+  $("#unit").html(unit);
+  if(unit === 'c') {
+    $("#temp").html(Math.floor(degreeInCelcius) + " °");
+  }
+  else {
+    $("#temp").html(Math.floor(degreeInFahrenheit) + " °");
+  }
+}
+
+function convertFromCToF(degree){
+  degree = Number(degree);
+  return Math.floor((degree*9 + 160)/5);
+}
