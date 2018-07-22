1a40fdc27df90eba67524030662aa3c128f07839
ppppbn <phambaongoc1511@gmail.com>
2018-07-22 13:11:04 +0700

    Change API & add search box for country

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ const weather = ['drizzle', 'clouds', 'rain', 'snow', 'clear', 'thunderstom'];
 var unit = "c";
 var degreeInCelcius = 0;
 var degreeInFahrenheit = 0;
+const APP_ID = "030bd2c5e874b35251001c10b023f1eb";
+const API_URL = "http://api.openweathermap.org/data/2.5/weather";
+const DEFAULT_UNIT = "metric"
+const DEFAULT_COUNTRY = "Hanoi";
 
 window.onload = function(){ 
   $(".cover-bg").fadeOut(500);  
@@ -13,47 +17,76 @@ $(document).ready(function(){
     toggleUnit();
   })
 
-  
+  $('#search-form').submit(function(e){
+    e.preventDefault();
+    const input = $('#search-input').val();
+    if (input) {
+      getWeather(null, input);
+    } else {
+      return;
+    }
+  })
 })
 
 function getLocation() {
   if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(getWeather, displayError);
+      navigator.geolocation.getCurrentPosition(getWeather, () => getWeather(null, null));
   } else {
-      $("#geolocation-info").html("Geolocation is not supported by this browser.");
-      $("#loading").fadeOut(200);
+    getWeather(null, null);
+    $("#loading").fadeOut(200);
   }
 }
 
-function getWeather(position) {
-  $.ajax({
-    url : "https://fcc-weather-api.glitch.me/api/current/",
-    data : {
-      lat : position.coords.latitude.toFixed(8),
-      lon : position.coords.longitude.toFixed(8)
-    },
-    type : "GET",
-    success: function(data){
-      degreeInCelcius = Math.floor(data.main.temp);
-      degreeInFahrenheit = convertFromCToF(degreeInCelcius);
-      $("#loading").fadeOut(200);
-      $("#country").html(data.name + ", " + data.sys.country);
-      $("#temp").html(Math.floor(data.main.temp) + " °");
-      $("#main").html(data.weather[0].main);
-      $("#unit").html(unit);
-      generateIcon(data.weather[0].main);
-      $(".info-container").fadeIn(400);
-    },
-    error: function(err){
-      console.log(err);
-    }
-  }) 
+function getWeather(position, country) {
+  const data = country ? {
+    q: country,
+    appid: APP_ID,
+    units: DEFAULT_UNIT
+  } : position ? {
+    lat : position.coords.latitude.toFixed(8),
+    lon : position.coords.longitude.toFixed(8),
+    appid: APP_ID,
+    units: DEFAULT_UNIT
+  } : {
+    q: DEFAULT_COUNTRY,
+    appid: APP_ID,
+    units: DEFAULT_UNIT
+  }
+  
+  loading(function(){
+    $.ajax({
+      url : API_URL,
+      data,
+      type : "GET",
+      success: function(data){
+        degreeInCelcius = Math.floor(data.main.temp);
+        degreeInFahrenheit = convertFromCToF(degreeInCelcius);
+        $("#loading").fadeOut(300);
+        $("#country").html(data.name + ", " + data.sys.country);
+        $("#temp").html(Math.floor(data.main.temp) + " °");
+        $("#main").html(data.weather[0].main);
+        $("#unit").html(unit);
+        generateIcon(data.weather[0].main);
+        $(".info-container").fadeIn(400);
+      },
+      error: function(err){
+        $("#loading").fadeOut(300, function(){
+          if(err.responseJSON && err.responseJSON.cod === '404'){
+            $("#error-message").html("City not found!");
+          } else {
+            $("#error-message").html("Something went wrong. Please try again.");
+          }
+        })
+      }
+    }) 
+  });
 }
 
-function displayError(error){
-  if (error.code == error.PERMISSION_DENIED)
-    $("#geolocation-info").html("You need to enable location services to see your local weather. Reload and try again!");
-    $("#loading").fadeOut(200);
+function loading(cb){
+  $("#error-message").html("");
+  $(".info-container").fadeOut(200, function(){
+    $("#loading").fadeIn(200, cb);
+  });
 }
 
 function generateIcon(desc) {
